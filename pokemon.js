2a232d130228e7a1e4bf250d02d69cef485b4437
bauzor <bjgau@outlook.com>
2020-12-23 18:58:06 -0500

    deleted the frontend

--- pokemon.js
+++ pokemon.js
@@ -33,6 +33,29 @@ const SELECTORS = {
     }
 }
 
+class Browser {
+    constructor() {
+        this.browser = await puppeteer.launch({
+            defaultViewport : {width : 1000, height : 1080},
+            headless : false,
+            slowMo : 50,
+            devtools : true,
+        });
+
+        this.page = await this.browser.newPage();
+
+        this.data = null;
+    }
+
+    go_to_troll_and_toad(page) {
+
+    }
+
+    go_to_tcgplayer(page) {
+
+    }
+}
+
 open_browser();
 
 async function open_browser() {
@@ -50,107 +73,3 @@ async function open_browser() {
     let tcg_data = await tcg_player(page, tt_data);
     console.log(tcg_data)
 }
-
-async function trollandtoad(page) {
-    
-    await page.goto(URLS.tt);
-    await page.waitForSelector(SELECTORS.TT.prod_group_menu);
-
-    await page.hover(SELECTORS.TT.prod_group_menu);
-    await page.click(SELECTORS.TT.pokemon_grouping);
-    await page.type(SELECTORS.TT.search_bar, TEST_CARDS.Charizard + '\u000d');
-    
-    await page.waitForSelector(SELECTORS.TT.card);
-    await page.click(SELECTORS.TT.card);
-
-    await page.waitForSelector('.d-flex.font-weight-bold.font-default.text-center.align-items-end');
-    await page.waitForSelector('.font-weight-bold.font-large.font-md-largest.mt-1');
-
-    let data = await page.evaluate(async(SELECTORS) => {
-        const card_name = /^.*?(?=-)/;
-        const card_rarity = /(?<=\-)(.*?)(?=\-)/;
-        let amount = document.querySelector(SELECTORS.TT.amount)
-            .textContent
-            .trim();
-        let pokemon_card = card_name.exec(document.querySelector(SELECTORS.TT.pokemon_card)
-            .textContent)[0]
-            .trim();
-        let card_num = card_rarity.exec(document.querySelector(SELECTORS.TT.pokemon_card)
-            .textContent)[0]
-            .trim();
-        let card_set = document.querySelector(SELECTORS.TT.card_set)
-            .textContent
-            .trim()
-        
-        if(amount.length > 7) {
-            amount = document.querySelector(SELECTORS.TT.sale_amount)
-                .textContent
-                .trim();
-        }
-        
-        data = {
-            Source : 'Troll and Toad',
-            Amount : amount,
-            Card_Name : pokemon_card,
-            Card_Number : card_num,
-            Card_Set : card_set,
-        };
-
-        return data;
-    }, SELECTORS);
-
-    return data;
-}
-
-async function tcg_player(page, tt_data) {
-    await page.goto(URLS.tcg);
-    await page.waitForSelector(SELECTORS.TCG.search_bar);
-
-    await page.type(SELECTORS.TCG.search_bar, tt_data.Card_Name + '\u000d');
-    await page.waitForSelector(SELECTORS.TCG.pagination);
-
-await page.evaluate(async (SELECTORS, tt_data) => {
-        const card_rarity = /(?<=#).*/;// Ask alex about card rarity numbers [A-Z]{1,2}\d{1,2}
-        let card_numbers = document.querySelectorAll(SELECTORS.TCG.card_nums);
-        for(let i = 0; i < card_numbers.length; i++) {
-            card_num = card_rarity.exec(card_numbers[i]
-                .textContent);
-            if(card_num == tt_data.Card_Number) {
-                return card_numbers[i].click()
-            }
-        }
-    }, SELECTORS, tt_data);
-
-    await page.waitForSelector(SELECTORS.TCG.add_to_cart)
-    
-    let data = await page.evaluate((SELECTORS) => {
-        const card_number = /[A-Z]*[0-9]*\/[A-Z]*[0-9]*/;
-        let amount = document.querySelector(SELECTORS.TCG.amount)
-            .textContent
-            .trim();
-        console.log(amount)
-        let pokemon_card = document.querySelector(SELECTORS.TCG.pokemon_card)
-            .textContent
-            .trim();
-        console.log(pokemon_card)
-        let card_set = document.querySelector(SELECTORS.TCG.card_set)
-            .textContent
-            .trim();
-        console.log(card_set)
-        let card_num = card_number.exec(document.querySelector(SELECTORS.TCG.card_num)
-            .textContent
-            .trim())[0];
-        console.log(card_num)
-
-        return {
-            Source : 'TCG Player',
-            Amount : amount,
-            Card_Name : pokemon_card,
-            Card_Number : card_num,
-            Card_Set : card_set,
-        }
-
-    }, SELECTORS);
-
-    return data;
-}   
--- tcg.js
+++ tcg.js
@@ -1,52 +1,53 @@
-async function tcg_player(page, tt_data) {
-    await page.goto(URLS.tcg);
-    await page.waitForSelector(SELECTORS.TCG.search_bar);
-
-    await page.type(SELECTORS.TCG.search_bar, tt_data.Card_Name + '\u000d');
-    await page.waitForSelector(SELECTORS.TCG.pagination);
-
-await page.evaluate(async (SELECTORS, tt_data) => {
-        const card_rarity = /(?<=#).*/;// Ask alex about card rarity numbers [A-Z]{1,2}\d{1,2}
-        let card_numbers = document.querySelectorAll(SELECTORS.TCG.card_nums);
-        for(let i = 0; i < card_numbers.length; i++) {
-            card_num = card_rarity.exec(card_numbers[i]
-                .textContent);
-            if(card_num == tt_data.Card_Number) {
-                return card_numbers[i].click()
-            }
-        }
-    }, SELECTORS, tt_data);
-
-    await page.waitForSelector(SELECTORS.TCG.add_to_cart)
+class TCGPlayer  {
     
-    let data = await page.evaluate((SELECTORS) => {
-        const card_number = /[A-Z]*[0-9]*\/[A-Z]*[0-9]*/;
-        let amount = document.querySelector(SELECTORS.TCG.amount)
-            .textContent
-            .trim();
-        console.log(amount)
-        let pokemon_card = document.querySelector(SELECTORS.TCG.pokemon_card)
-            .textContent
-            .trim();
-        console.log(pokemon_card)
-        let card_set = document.querySelector(SELECTORS.TCG.card_set)
-            .textContent
-            .trim();
-        console.log(card_set)
-        let card_num = card_number.exec(document.querySelector(SELECTORS.TCG.card_num)
-            .textContent
-            .trim())[0];
-        console.log(card_num)
+    constructor(browser) {
 
-        return {
-            Source : 'TCG Player',
-            Amount : amount,
-            Card_Name : pokemon_card,
-            Card_Number : card_num,
-            Card_Set : card_set,
-        }
+    }
 
-    }, SELECTORS);
-
-    return data;
-}   
+    async function tcg_player(page, tt_data) {
+        await page.goto(URLS.tcg);
+        await page.waitForSelector(SELECTORS.TCG.search_bar);
+        await page.type(SELECTORS.TCG.search_bar, tt_data.Card_Name + '\u000d');
+        await page.waitForSelector(SELECTORS.TCG.pagination);
+        await page.evaluate(async (SELECTORS, tt_data) => {
+            // Ask alex about card rarity numbers [A-Z]{1,2}\d{1,2}
+            const card_rarity = /(?<=#).*/;
+            let card_numbers = document.querySelectorAll(SELECTORS.TCG.card_nums);
+            for (let i = 0; i < card_numbers.length; i++) {
+                card_num = card_rarity.exec(card_numbers[i].textContent);
+                if (card_num == tt_data.Card_Number) {
+                    return card_numbers[i].click()
+                }
+            }
+        }, SELECTORS, tt_data);
+        await page.waitForSelector(SELECTORS.TCG.add_to_cart)
+        let data = await page.evaluate((SELECTORS) => {
+            const card_number = /[A-Z]*[0-9]*\/[A-Z]*[0-9]*/;
+            let amount = document
+                .querySelector(SELECTORS.TCG.amount)
+                .textContent.trim();
+            console.log(amount)
+            let pokemon_card = document
+                .querySelector(SELECTORS.TCG.pokemon_card)
+                .textContent.trim();
+            console.log(pokemon_card)
+            let card_set = document
+                .querySelector(SELECTORS.TCG.card_set)
+                .textContent.trim();
+            console.log(card_set)
+            let card_num = card_number.exec(
+                document.querySelector(SELECTORS.TCG.card_num).textContent.trim()
+            )[0];
+            console.log(card_num)
+    
+            return {
+                Source : 'TCG Player',
+                Amount : amount,
+                Card_Name : pokemon_card,
+                Card_Number : card_num,
+                Card_Set : card_set,
+            };
+        }, SELECTORS);
+        return data;
+    } 
+}
